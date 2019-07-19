64def8f6d3ec5e202231b4faeb8993c50527e437
narendra <root@nare.com>
2019-07-19 06:49:30 +0000

    6

--- new.sh
+++ new.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-<<<<<<< HEAD
+#<<<<<<< HEAD
 echo "What is your Name"
 read Name
 echo "Hi how are you $Name?"
 read answer
 echo "me to $answer?!"
-=======
-#echo "What is your Name"
-#read Name
-#echo "Hi how are you $Name?"
-#read answer
-#echo "me to $answer?!"
->>>>>>> c9f9a8296d42b292fff9f65ffcf50eec96508f3c
 
 echo 'Welcome'
 
